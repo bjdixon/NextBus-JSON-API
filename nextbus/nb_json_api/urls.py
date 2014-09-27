3b22b4fde4e5cb29c66156193ceff0fea1ac6335
Brian J Dixon <bjdixon@gmail.com>
2014-09-27 16:54:33 -0400

    added view to output agencies

--- nextbus/nb_json_api/urls.py
+++ nextbus/nb_json_api/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
+
 admin.autodiscover()
 
+
 urlpatterns = patterns('',
-    url(r'^api/', include('nb_json_api.urls')),
-    url(r'^admin/', include(admin.site.urls)),
+        url(r'^agency_list/$', 'nb_json_api.views.view_agency_list', name='agency_list'),
 )
+
--- nextbus/nb_json_api/views.py
+++ nextbus/nb_json_api/views.py
@@ -1,1 +1,21 @@
-# Create your views here.
+import urllib2
+
+from django.utils import simplejson
+from django.http import HttpResponse
+from bs4 import BeautifulSoup
+
+
+AGENCY_LIST_URL = 'http://webservices.nextbus.com/service/publicXMLFeed?command=agencyList'
+
+def view_agency_list(request):
+    soup = BeautifulSoup(urllib2.urlopen(AGENCY_LIST_URL).read())
+    agency_list = []
+    for agency in soup.find_all('agency'):
+        agency_list.append({
+                'title': agency['title'],
+                'id': agency['tag'],
+                'region': agency['regiontitle'],
+                })
+    data = simplejson.dumps(agency_list)
+    return HttpResponse(data, mimetype='application/json')
+
